7866a510e4754bf96f30899fc9b5ad544c9d24df
salsabeel <s.jaafreh@yahoo.com>
2018-09-04 13:56:37 +0300

    counter

--- ng-count/ng-count.js
+++ ng-count/ng-count.js
@@ -1,3 +1,30 @@
-angular.module('count', []);
 
-angular.module('count')
+var count=angular.module('count', []);
+
+count.factory('UserService', function() {
+  return {
+      number: ''
+  };
+});
+
+count.controller('countCtrl',function($scope,UserService){
+
+$scope.number = 0;
+
+})
+
+
+count.controller('incrCtrl',function($scope,UserService){
+// $scope.number = 0;
+     
+   
+});
+
+count.controller('decrCtrl',function($scope,UserService){
+        $scope.decrement = function() {
+            $scope.number--;
+        };
+});
+
+
+
--- node-track-bools/server.js
+++ node-track-bools/server.js
@@ -12,19 +12,42 @@ http.createServer(function(request, response) {
 
   if (method === 'POST' && pathname !== '/') {
     
-  } else if (method === 'GET') {
-  
+  } 
+
+
+  else if (method === 'GET') {
     // Don't forget to serve the index.html
+  fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
 
     if (pathname === '/') {
       //your code is here 
+   if (globalObject[this.id] == 0 ) { 
+                console.log("toggle to 1");
+                globalObject[this.id] = 1;
+            } else {
+                globalObject[this.id] = 0;
+                console.log("toggle to 0");
+            }
+
      
     } else {
      // your code is here
+     //i cant understand what this else should do
       
     }
-  } else {
+  }
+
+
+
+
+  else {
     //your code is here 
+    //this else for error
+    console.log('error')
     
   }
  
